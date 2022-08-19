afad1c1f1b70fc493f3fbbbc8293d569910c7921
andrejnascimento <ak.vips@gmail.com>
2022-08-19 15:07:29 -0300

    Configuração da janela etapa2

--- main.py
+++ main.py
@@ -2,8 +2,25 @@
 from tkinter import *
 from tkinter import ttk
 
+# cores
+
+cor1 = "#1e1f1e" # preta
+cor2 = "feffff" # branca
+cor3 = "#38576b" # azul
+cor4 = "#ECEFF1" # cinza
+cor5 = "#FFAB40" # laranja
+
+
 janela =Tk()
 janela.title("Calculadora")
+janela.geometry("235x318")
+janela.config(bg=cor1)
+
+frame_tela = Frame(janela, width=235, height=50)
+frame_tela.grid(row=0, column=0)
+
+frame_corpo = Frame(janela, width=235, height=268)
+frame_corpo.grid(row=1, column=0)
 
 janela.mainloop()
 
